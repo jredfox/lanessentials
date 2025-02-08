48838eed776b63b2eea9ac8e263ec60ec665bf69
jredfox <jredfox_@outlook.com>
2025-02-08 00:40:12 -0600

    handle error

--- src/main/java/com/jredfox/lanessentials/commands/cap/CommandSpeedFly.java
+++ src/main/java/com/jredfox/lanessentials/commands/cap/CommandSpeedFly.java
@@ -1,11 +1,13 @@
 package com.jredfox.lanessentials.commands.cap;
 
+import com.evilnotch.lib.util.JavaUtil;
 import com.jredfox.lanessentials.cap.CapAbility;
 import com.jredfox.lanessentials.cap.CapHandler;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 
@@ -26,6 +28,10 @@ public class CommandSpeedFly extends CommandBase
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 		if(!(sender instanceof EntityPlayerMP))
 			return;
+		
+		if(args.length > 0 && !JavaUtil.isStringNum(args[0]))
+			throw new WrongUsageException("speedFly must be a float!",new Object[0]);
+		
 		CapAbility c = CapHandler.getCapAbility((EntityPlayerMP) sender);
 		c.speedFly = args.length == 0 ? 0 : Float.parseFloat(args[0]);
 		c.sync((EntityPlayerMP) sender);
